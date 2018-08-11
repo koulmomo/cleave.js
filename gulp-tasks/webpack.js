9fddea9c9fb0ff0f8f51d597f86a53620807e812
Mo Kouli <mkouli@uber.com>
2018-08-10 21:29:09 -0700

    :art: es6 Cleave.react.js

--- gulp-tasks/webpack.js
+++ gulp-tasks/webpack.js
@@ -4,100 +4,127 @@ var path = require('path');
 var concat = require('gulp-concat');
 
 var paths = {
-    tmp: './tmp',
-    root: './',
-    dist: './dist/',
-    src: './src/'
+  tmp: './tmp',
+  root: './',
+  dist: './dist/',
+  src: './src/'
 };
 
-gulp.task('js:vanilla', function () {
-    return gulp.src(path.join(paths.src + 'Cleave.js'))
-        .pipe(webpack({
-            output: {
-                library: 'Cleave',
-                libraryTarget: 'umd',
-                filename: 'cleave.js'
-            }
-        }))
-        .pipe(gulp.dest(paths.dist));
+gulp.task('js:vanilla', function() {
+  return gulp
+    .src(path.join(paths.src + 'Cleave.js'))
+    .pipe(
+      webpack({
+        output: {
+          library: 'Cleave',
+          libraryTarget: 'umd',
+          filename: 'cleave.js'
+        }
+      })
+    )
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('js:angular-merge', function () {
-    return gulp.src([
-            path.join(paths.src, 'Cleave.js'),
-            path.join(paths.src, 'Cleave.angular.js')
-        ])
-        .pipe(concat('cleave-angular.js'))
-        .pipe(gulp.dest(paths.tmp));
+gulp.task('js:angular-merge', function() {
+  return gulp
+    .src([
+      path.join(paths.src, 'Cleave.js'),
+      path.join(paths.src, 'Cleave.angular.js')
+    ])
+    .pipe(concat('cleave-angular.js'))
+    .pipe(gulp.dest(paths.tmp));
 });
 
-gulp.task('js:angular', function () {
-    return gulp.src(path.join(paths.tmp, 'cleave-angular.js'))
-        .pipe(webpack({
-            output: {
-                library: 'Cleave',
-                libraryTarget: 'umd',
-                filename: 'cleave-angular.js'
-            }
-        }))
-        .pipe(gulp.dest(paths.dist));
+gulp.task('js:angular', function() {
+  return gulp
+    .src(path.join(paths.tmp, 'cleave-angular.js'))
+    .pipe(
+      webpack({
+        output: {
+          library: 'Cleave',
+          libraryTarget: 'umd',
+          filename: 'cleave-angular.js'
+        }
+      })
+    )
+    .pipe(gulp.dest(paths.dist));
 });
 
 var module = {
-    loaders: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015', 'react', 'stage-0']
-            }
-        }
-    ]
+  loaders: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react', 'stage-0']
+      }
+    }
+  ]
 };
 
 var externals = [
-    {
-        'react': {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react'
-        },
-        'react-dom': {
-            root: 'ReactDOM',
-            commonjs2: 'react-dom',
-            commonjs: 'react-dom',
-            amd: 'react-dom'
-        }
+  {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
     }
+  },
+  'preact-compat'
 ];
 
-gulp.task('js:react-node', function () {
-    return gulp.src(path.join(paths.src, 'Cleave.react.js'))
-        .pipe(webpack({
-            output: {
-                library: 'Cleave',
-                libraryTarget: 'umd',
-                filename: 'cleave-react-node.js'
-            },
-            target: 'node',
-            module: module,
-            externals: externals
-        }))
-        .pipe(gulp.dest(paths.dist));
+var resolve = {
+  alias: {
+    react: 'preact-compat',
+    'react-dom': 'preact-compat',
+    // Not necessary unless you consume a module using `createClass`
+    'create-react-class': 'preact-compat/lib/create-react-class',
+    // Not necessary unless you consume a module requiring `react-dom-factories`
+    'react-dom-factories': 'preact-compat/lib/react-dom-factories'
+  }
+};
+
+gulp.task('js:react-node', function() {
+  return gulp
+    .src(path.join(paths.src, 'Cleave.react.js'))
+    .pipe(
+      webpack({
+        output: {
+          library: 'Cleave',
+          libraryTarget: 'umd',
+          filename: 'cleave-react-node.js'
+        },
+        target: 'node',
+        module: module,
+        externals: externals,
+        resolve: resolve
+      })
+    )
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('js:react', function () {
-    return gulp.src(path.join(paths.src, 'Cleave.react.js'))
-        .pipe(webpack({
-            output: {
-                library: 'Cleave',
-                libraryTarget: 'umd',
-                filename: 'cleave-react.js'
-            },
-            module: module,
-            externals: externals
-        }))
-        .pipe(gulp.dest(paths.dist));
+gulp.task('js:react', function() {
+  return gulp
+    .src(path.join(paths.src, 'Cleave.react.js'))
+    .pipe(
+      webpack({
+        output: {
+          library: 'Cleave',
+          libraryTarget: 'umd',
+          filename: 'cleave-react.js'
+        },
+        module: module,
+        externals: externals,
+        resolve: resolve
+      })
+    )
+    .pipe(gulp.dest(paths.dist));
 });
